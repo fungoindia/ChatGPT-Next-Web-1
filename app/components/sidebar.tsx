7784916d94598a781294a45e8b76afc8cf023d04
fungoindia <83899086+fungoindia@users.noreply.github.com>
2023-06-13 14:24:21 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT GPT4 For Free</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AI麒麟-唯一免费、无限制GPT4
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
